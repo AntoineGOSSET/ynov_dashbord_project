443e5f79225a077eef4dd17603185da62e3c8481
Kevin <kevin.pautonnier@ynov.com>
2016-12-13 18:25:27 +0100

    coucou

--- YnovDashboardProject/YnovDashboardProject/App.xaml.cs
+++ YnovDashboardProject/YnovDashboardProject/App.xaml.cs
@@ -17,6 +17,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace YnovDashboardProject
 {
+    //COUCOU
     /// <summary>
     /// Fournit un comportement spécifique à l'application afin de compléter la classe Application par défaut.
     /// </summary>
